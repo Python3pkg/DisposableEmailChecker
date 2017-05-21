fb29d45502e56ab5a8420c42d7ff54b275ec230a
Python3pkg <raliclo@gmail.com>
2017-05-21 05:53:59 -0700

    Convert to Python3

--- build_list.py
+++ build_list.py
@@ -2,4 +2,4 @@
 emails = sorted(set([line.strip() for line in open("email_domains.txt")]))
 
 for email in emails:
-    print("'{email}',".format(email=email))
+    print(("'{email}',".format(email=email)))
